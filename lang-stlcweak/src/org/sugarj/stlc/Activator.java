daf8d479ad5367e9c6ad094d81adc45a36fc168f
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-03-19 13:12:04 +0100

    stlc -> stlcweak

--- lang-stlcweak/src/org/sugarj/stlc/Activator.java
+++ lang-stlcweak/src/org/sugarj/stlc/Activator.java
@@ -1,4 +1,4 @@
-package org.sugarj.stlc;
+package org.sugarj.stlcweak;
 
 import org.sugarj.soundx.SoundXActivator;
 
@@ -6,8 +6,8 @@ import org.sugarj.soundx.SoundXActivator;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends SoundXActivator {
-	public static final String PLUGIN_ID = "org.sugarj.stlc";
+	public static final String PLUGIN_ID = "org.sugarj.stlcweak";
 	public Activator() {
-		super("STLC.sxbld");
+		super("STLCWeak.sxbld");
 	}
 }
