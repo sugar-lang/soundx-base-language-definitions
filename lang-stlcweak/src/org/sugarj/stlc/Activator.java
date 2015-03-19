6524f85e294c819aa57f92d208fd45f8cb32e531
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-03-19 13:09:11 +0100

    STLC with weakening rule

---
+++ lang-stlcweak/src/org/sugarj/stlc/Activator.java
@@ -0,0 +1,13 @@
+package org.sugarj.stlc;
+
+import org.sugarj.soundx.SoundXActivator;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends SoundXActivator {
+	public static final String PLUGIN_ID = "org.sugarj.stlc";
+	public Activator() {
+		super("STLC.sxbld");
+	}
+}
