d4ad0349ce33163747f2b57776ef6ea5dfcbd0f1
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-03-19 12:52:07 +0100

    Added bogus base language definition

---
+++ lang-bogus/src/org/sugarj/bogus/Activator.java
@@ -0,0 +1,13 @@
+package org.sugarj.bogus;
+
+import org.sugarj.soundx.SoundXActivator;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends SoundXActivator {
+	public static final String PLUGIN_ID = "org.sugarj.bogus";
+	public Activator() {
+		super("Bogus.sxbld");
+	}
+}
