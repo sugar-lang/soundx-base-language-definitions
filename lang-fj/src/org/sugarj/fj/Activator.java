ae5c9ade8a143ee71715d889df055405d374379b
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-04-30 20:40:02 +0200

    Project for FJ.

---
+++ lang-fj/src/org/sugarj/fj/Activator.java
@@ -0,0 +1,10 @@
+package org.sugarj.fj;
+
+import org.sugarj.soundx.SoundXActivator;
+
+public class Activator extends SoundXActivator {
+	public static final String PLUGIN_ID = "org.sugarj.fj";
+	public Activator() {
+		super("FJ.sxbld");
+	}
+}
