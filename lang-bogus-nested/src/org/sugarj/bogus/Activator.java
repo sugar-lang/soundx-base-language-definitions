c2822d18b7d92db03b6a262e66a426010ff673f5
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-05-05 11:52:33 +0200

    Bogus base language to test nested namespaces.

---
+++ lang-bogus-nested/src/org/sugarj/bogus/Activator.java
@@ -0,0 +1,13 @@
+package org.sugarj.bogusnested;
+
+import org.sugarj.soundx.SoundXActivator;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends SoundXActivator {
+	public static final String PLUGIN_ID = "org.sugarj.bogusnested";
+	public Activator() {
+		super("BogusNested.sxbld");
+	}
+}
