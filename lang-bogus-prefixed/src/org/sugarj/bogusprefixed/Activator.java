72c413e80eb146a9c265e0b5e0b9744287872f2e
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-05-08 10:52:07 +0200

    Bogus language to test prefixed namespaces.

---
+++ lang-bogus-prefixed/src/org/sugarj/bogusprefixed/Activator.java
@@ -0,0 +1,13 @@
+package org.sugarj.bogusprefixed;
+
+import org.sugarj.soundx.SoundXActivator;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends SoundXActivator {
+	public static final String PLUGIN_ID = "org.sugarj.bogusprefixed";
+	public Activator() {
+		super("BogusPrefixed.sxbld");
+	}
+}
