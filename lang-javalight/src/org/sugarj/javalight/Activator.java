aafc42059d493a9a63db56a968e16a9eff04f468
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2015-06-26 13:53:08 +0200

    FJ -> JavaLight

---
+++ lang-javalight/src/org/sugarj/javalight/Activator.java
@@ -0,0 +1,10 @@
+package org.sugarj.javalight;
+
+import org.sugarj.soundx.SoundXActivator;
+
+public class Activator extends SoundXActivator {
+	public static final String PLUGIN_ID = "org.sugarj.javalight";
+	public Activator() {
+		super("JavaLight.sxbld");
+	}
+}
